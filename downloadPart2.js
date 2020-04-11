91f07a1e04d04e78cb3384eeaf26437589a004aa
MeatReed <tim.oumpho@gmail.com>
2020-04-11 13:09:08 +0200

    add part download

---
+++ downloadPart2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const Manga = require('./smallMangaRock');
+const axios = require('axios')
+
+if (!fs.existsSync('output')) {
+	fs.mkdirSync('output')
+}
+
+axios
+  .post(
+    `https://web.mangarockhd.com/query/web450/mrs_filter?country=France`,
+    {
+      status: 'all',
+      genres: {},
+      rank: 'all',
+      order: 'rank',
+    }
+  )
+  .then(async (oidsResponse) => {
+    for (let i = 0; i < oidsResponse.data.data.slice(oidsResponse.data.data.length / 2, oidsResponse.data.data.length).length; i++) {
+      await Manga.downloadManga(oidsResponse.data.data[i])
+      console.log(`\tDone oid: ${oidsResponse.data.data[i]}`)
+    }
+    console.log('Done !!')
+  })
